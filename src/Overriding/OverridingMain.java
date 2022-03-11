3edde786c634aadc665a9a47497540dae8b650c8
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-03-11 22:08:34 +0530

    Changes made to describe method hiding in Child Class.

--- src/Overriding/OverridingMain.java
+++ src/Overriding/OverridingMain.java
@@ -4,5 +4,14 @@ public class OverridingMain {
     public static void main(String[] args) {
         Parent.m1();
         Children.m1();
+
+        Parent p = new Parent();
+        p.m1();
+
+        Children c = new Children();
+        c.m1();
+
+        Parent p1 = new Children();
+        p1.m1();
     }
 }
