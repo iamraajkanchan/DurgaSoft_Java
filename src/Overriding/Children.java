3ee927de4e5aa4ddb7cfb862ca666a75ab9fdd91
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-03-11 13:23:34 +0530

    Changes made to describe role of exception case in child class in Overriding Concept

--- src/Overriding/Children.java
+++ src/Overriding/Children.java
@@ -1,8 +1,48 @@
 package Overriding;
 
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Children extends Parent {
     @Override
     public void m1() {
-        System.out.println("Implement Child Method");
+        System.out.println("Child Class Method");
+    }
+
+    /* Prompts Compile Time Exception */
+    /*
+    @Override
+    public void m2() throws Exception {
+        System.out.println("Child Class Method that throws Exception");
+    }
+    */
+
+    @Override
+    public void m3() throws IOException {
+        System.out.println("Child Class Method that throws IOException");
+    }
+
+    /* Prompts Compile Time Exception */
+    /*
+    @Override
+    public void m4() throws Exception {
+        System.out.println("Child Class Method that throws Exception");
+    }
+    */
+
+    @Override
+    public void m5() throws FileNotFoundException, EOFException {
+        System.out.println("Child Class Method that throws FileNotFoundException and EOFException");
+    }
+
+    @Override
+    public void m6() throws EOFException {
+        System.out.println("Child Class Method that throws EOFException");
+    }
+
+    @Override
+    public void m7() throws ArithmeticException, NullPointerException, ClassCastException {
+        System.out.println("Child Class Method that throws, ArithmeticException, NullPointerException, ClassCastException");
     }
 }
--- src/Overriding/Parent.java
+++ src/Overriding/Parent.java
@@ -1,7 +1,33 @@
 package Overriding;
 
+import java.io.IOException;
+
 public class Parent {
-    void m1() {
-        System.out.println("Implement Parent Method");
+    public void m1() throws Exception {
+        System.out.println("Parent Class Method that throws Exception");
+    }
+
+    public void m2() {
+        System.out.println("Parent Class Method");
+    }
+
+    public void m3() throws Exception {
+        System.out.println("Parent Class Method that throws Exception");
+    }
+
+    public void m4() throws IOException {
+        System.out.println("Parent Class Method that throws IOException");
+    }
+
+    public void m5() throws IOException {
+        System.out.println("Parent Class Method that throws IOException");
+    }
+
+    public void m6() throws IOException {
+        System.out.println("Parent Class Method that throws IOException");
+    }
+
+    public void m7() throws IOException {
+        System.out.println("Parent Class Method that throws IOException");
     }
 }
