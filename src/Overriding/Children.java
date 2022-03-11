de7cd694251ae12a623c8ca1b771d1cf8d20b4fb
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-03-11 23:59:18 +0530

    Changes made to describe var-arg case in Child Class.

--- src/Overriding/Children.java
+++ src/Overriding/Children.java
@@ -4,6 +4,6 @@ public class Children extends Parent {
     /* This is an example of Overloading but not Overriding */
     /* This is an instance level method */
     public void m1(int x) {
-        System.out.println("Child class method with int type argument.");
+        System.out.println("Child class method with int type argument");
     }
 }
