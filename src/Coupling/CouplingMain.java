2629ddbaefdc3432d0f2b1bfeec2da582f347cb5
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-03-13 01:49:12 +0530

    Changes made to describe Tight Coupling

--- src/Coupling/CouplingMain.java
+++ src/Coupling/CouplingMain.java
@@ -3,7 +3,5 @@ package Coupling;
 public class CouplingMain {
     public static void main(String[] args) {
         System.out.println(A.i);
-        Object o = new String("Raj Kanchan");
-        Runnable r = new Thread();
     }
 }
