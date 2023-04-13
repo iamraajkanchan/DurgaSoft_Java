53c2e658a21d72a9aa1ee651a3ce7070dcd7957e
Raj K <raj.k@mobicule.com>
2023-04-13 19:27:06 +0530

    InnerClassExample.java is created in InnerClass package.

---
+++ src/InnerClass/InnerClassExample.java
@@ -0,0 +1,44 @@
+package InnerClass;
+
+public class InnerClassExample {
+    public String a = "anushka";
+
+    public class MyHome {
+        void cook() {
+            System.out.println(a + " is cooking");
+        }
+
+        public class BrotherRoom {
+
+            public class WifeMirror {
+                String mirrorName = "Gold Round Mirror";
+
+                void makeup() {
+                    System.out.println(a + " is doing make up on a drawer");
+                }
+            }
+
+            public static class WifeDrawer {
+                String taylor = "Taylor Swift";
+
+                void makeup() {
+                    System.out.println(taylor + " is doing make up on a drawer");
+                }
+
+            }
+
+
+        }
+
+        public class FatherRoom {
+            public class WifeDrawer {
+
+            }
+        }
+
+        public class MotherRoom {
+
+        }
+
+    }
+}
