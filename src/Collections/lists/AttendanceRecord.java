420e354693f3a7b66676eac52680f56c66e4cb10
Raj K <raj.k@mobicule.com>
2023-05-18 18:17:11 +0530

    AttendanceRecord.java is added in Collection/lists package.

---
+++ src/Collections/lists/AttendanceRecord.java
@@ -0,0 +1,14 @@
+package Collections.lists;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class AttendanceRecord{
+
+	@SerializedName("AttendanceRecord")
+	private List<AttendanceRecordItem> attendanceRecord;
+
+	public List<AttendanceRecordItem> getAttendanceRecord(){
+		return attendanceRecord;
+	}
+}
