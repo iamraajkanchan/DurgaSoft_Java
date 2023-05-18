25475729594a5e567eee8342bec861a2447bb542
Raj K <raj.k@mobicule.com>
2023-05-18 18:19:38 +0530

    CarType.java is updated.

--- src/InterfaceAndAbstraction/AbstractFactoryPattern/CarType.java
+++ src/InterfaceAndAbstraction/AbstractFactoryPattern/CarType.java
@@ -1,6 +1,14 @@
 package InterfaceAndAbstraction.AbstractFactoryPattern;
 
 public enum CarType {
-    MICRO, MINI, LUXURY
+    MICRO(), MINI, LUXURY;
+
+    void update(String value) {
+        MICRO(value);
+    }
+
+    private void MICRO(String value) {
+
+    }
 }
 
