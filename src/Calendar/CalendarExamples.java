0f26e3a5bcb485b4e8078f1d94064d18a11b7491
Raj K <raj.k@mobicule.com>
2023-05-18 13:54:52 +0530

    CalendarExamples.java is created in Calendar package.

---
+++ src/Calendar/CalendarExamples.java
@@ -0,0 +1,28 @@
+package Calendar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class CalendarExamples {
+    public static void main(String... args) {
+        SimpleDateFormat sf = new SimpleDateFormat("dd-MM-yy", Locale.ENGLISH);
+
+        Calendar fromCalendar = Calendar.getInstance();
+        fromCalendar.add(Calendar.DAY_OF_YEAR, -15);
+        String fromCalendarString = sf.format(fromCalendar.getTime());
+        System.out.println("From Date: " + fromCalendarString);
+
+        String toCalendarString = sf.format(Calendar.getInstance().getTime());
+        System.out.println("To Date: " + toCalendarString);
+
+        System.out.println("Attendance ID " + getAttendanceId());
+        System.out.println("Attendance ID Length " + getAttendanceId().length());
+    }
+
+    private static String getAttendanceId() {
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss", Locale.ENGLISH);
+        String dateString = sdf.format(Calendar.getInstance().getTime());
+        return "7718881920" + dateString;
+    }
+}
