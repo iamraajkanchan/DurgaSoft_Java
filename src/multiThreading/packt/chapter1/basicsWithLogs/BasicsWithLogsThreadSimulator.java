b9cc91779539319631c2b735717d25ba1dcca712
Raaj Kanchan <raj.k@mobicule.com>
2023-07-11 12:37:03 +0530

    BasicsWithLogsThreadSimulator is updated in packt package.

--- src/multiThreading/packt/chapter1/basicsWithLogs/BasicsWithLogsThreadSimulator.java
+++ src/multiThreading/packt/chapter1/basicsWithLogs/BasicsWithLogsThreadSimulator.java
@@ -1,8 +1,10 @@
 package multiThreading.packt.chapter1.basicsWithLogs;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
 public class BasicsWithLogsThreadSimulator {
     public static void main(String... args) {
@@ -16,22 +18,57 @@ public class BasicsWithLogsThreadSimulator {
                 threads[i].setPriority(Thread.MIN_PRIORITY);
             }
             threads[i].setName("Thread " + i);
-            threads[i].start();
         }
         try {
-            FileWriter file = new FileWriter(".\\data\\logs.txt");
-            PrintWriter pw = new PrintWriter(file);
-            for(int i = 0; i < 10; i++) {
-                pw.println("Main: Status of Thread " + i + threads[i].getState());
-                status[i] = threads[i].getState();
-                threads[i].start();
-                boolean finish = false;
-                while (!finish) {
-
+            final File dir = new File("data");
+            if (!dir.exists()) {
+                if (!dir.mkdir()) {
+                    System.out.println("Folder Not Found!!!");
+                    System.exit(0);
+                } else {
+                    processIfDirectoryExist(dir, threads, status);
                 }
+            } else {
+                processIfDirectoryExist(dir, threads, status);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void processIfDirectoryExist(File dir, Thread[] threads, Thread.State[] status) throws IOException {
+        FileWriter file = new FileWriter(new File(dir, "logs.txt"));
+        PrintWriter pw = new PrintWriter(file);
+        for (int i = 0; i < 10; i++) {
+            pw.printf("Main: Status of Thread %d %s\n", i, threads[i].getState());
+            status[i] = threads[i].getState();
+        }
+        pw.printf("Main : *****************************************************************\n");
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+        boolean finish = false;
+        while (!finish) {
+            for (int i = 0; i < 10; i++) {
+                if (threads[i].getState() != status[i]) {
+                    writeThreadInfo(pw, threads[i], status[i]);
+                    status[i] = threads[i].getState();
+                }
+                finish = true;
+            }
+            for (int i = 0; i < 10; i++) {
+                finish = finish && (threads[i].getState() == Thread.State.TERMINATED);
+            }
+        }
+        new Thread(pw::close).start();
+    }
+
+    private static void writeThreadInfo(PrintWriter pw, Thread thread, Thread.State state) {
+        pw.printf("Main : ID %d - %s\n", thread.threadId(), thread.getName());
+        pw.printf("Main : Priority - %d\n", thread.getPriority());
+        pw.printf("Main : Old State - %s\n", state);
+        pw.printf("Main : New State - %s\n", thread.getState());
+        pw.printf("Main : *****************************************************************\n");
+
     }
 }
