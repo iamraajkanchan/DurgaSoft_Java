d3912123bc5a87da297443921e48bbfa1e6785c3
Raaj Kanchan <raj.k@mobicule.com>
2023-07-13 12:17:13 +0530

    threadGroupException package is added in packt package.

--- src/multiThreading/packt/threadManagement/threadGroupException/ThreadGroupExceptionTask.java
+++ src/multiThreading/packt/threadManagement/threadGroupException/ThreadGroupExceptionTask.java
@@ -8,7 +8,7 @@ public class ThreadGroupExceptionTask implements Runnable {
         final Random random = new Random(Thread.currentThread().threadId());
         while (true) {
             final int result = 1000 / (int) (random.nextDouble() * 1000);
-            System.out.printf("%d : %f\n", Thread.currentThread().threadId(), result);
+            System.out.printf("%d : %d\n", Thread.currentThread().threadId(), result);
             if (Thread.currentThread().isInterrupted()) {
                 System.out.printf("%d : Interrupted\n", Thread.currentThread().threadId());
                 return;
