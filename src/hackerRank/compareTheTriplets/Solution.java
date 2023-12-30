9564406da47c64a7e8f87216962b399a67a9b3e8
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-30 15:55:09 +0530

    CompareTheTriplets problem is resolved. Result.java and Solution.java is added in th compareTheTriplets package.

---
+++ src/hackerRank/compareTheTriplets/Solution.java
@@ -0,0 +1,37 @@
+package hackerRank.compareTheTriplets;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+        for (int value : result) {
+            System.out.println(value);
+        }
+        /*
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+        */
+        bufferedReader.close();
+        // bufferedWriter.close();
+    }
+}
