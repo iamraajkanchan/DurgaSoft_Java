fee300c2f8959ae062b8e079f2ab334e1bfd8d5e
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-30 14:19:01 +0530

    hackerRank package is added. FizzBuzz problem is resolved. Result.java and Solution.java is added in th FizzBuzz package.

---
+++ src/hackerRank/fizzbuzz/Result.java
@@ -0,0 +1,25 @@
+package hackerRank.fizzbuzz;
+
+public class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 && i % 5 > 0) {
+                System.out.println("Fizz");
+            } else if (i % 3 > 0 && i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
