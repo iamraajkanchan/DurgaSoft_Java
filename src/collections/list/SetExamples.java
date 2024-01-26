1352d268af98a7c67e8308cef4ea36fa028eb75c
Raj Kanchan <iamraajkanchan@gmail.com>
2024-01-26 14:41:03 +0530

    Example of HashSet, LinkedHashSet, SortedSet, NavigableSet and TreeSet is added in SetExamples.java

--- src/collections/list/SetExamples.java
+++ src/collections/list/SetExamples.java
@@ -1,33 +1,63 @@
 package collections.list;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class SetExamples {
     public static void main(String... a) {
-        LinkedHashSet<Integer> numberLinkedHashSet =  new LinkedHashSet<>();
-        numberLinkedHashSet.add(1);
-        numberLinkedHashSet.add(3);
-        numberLinkedHashSet.add(2);
-        numberLinkedHashSet.add(2);
-        numberLinkedHashSet.add(6);
-        numberLinkedHashSet.add(5);
-        numberLinkedHashSet.add(7);
-        numberLinkedHashSet.add(4);
-        System.out.println("Example of LinkedHashSet");
-        numberLinkedHashSet.forEach(System.out::print);
-        System.out.println();
+        hashSetExample();
+        linkedHashSetExample();
+        sortedSetExample();
+        navigableSetExample();
+        treeSetExample();
+    }
+
+    private static void hashSetExample() {
         HashSet<Integer> numberHashSet = new HashSet<>();
-        numberHashSet.add(1);
-        numberHashSet.add(3);
-        numberHashSet.add(2);
-        numberHashSet.add(2);
-        numberHashSet.add(6);
-        numberHashSet.add(5);
-        numberHashSet.add(7);
-        numberHashSet.add(4);
+        populateSet(numberHashSet);
         System.out.println("Example of HashSet");
-        numberHashSet.forEach(System.out::print);
+        numberHashSet.stream().map(e -> e + " ").forEach(System.out::print);
         System.out.println();
+    }
+
+    private static void linkedHashSetExample() {
+        LinkedHashSet<Integer> numberLinkedHashSet = new LinkedHashSet<>();
+        populateSet(numberLinkedHashSet);
+        System.out.println("Example of LinkedHashSet");
+        numberLinkedHashSet.stream().map(e -> e + " ").forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void sortedSetExample() {
+        SortedSet<Integer> numberSortedSet = new TreeSet<>();
+        populateSet(numberSortedSet);
+        System.out.println("Example of SortedSet");
+        numberSortedSet.parallelStream().map(e -> e + " ").forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void navigableSetExample() {
+        NavigableSet<Integer> numberNavigableSet = new TreeSet<>();
+        populateSet(numberNavigableSet);
+        System.out.println("Example of NavigableSet");
+        numberNavigableSet.parallelStream().map(e -> e + " ").forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void treeSetExample() {
+        TreeSet<Integer> numberTreeSet = new TreeSet<>();
+        populateSet(numberTreeSet);
+        System.out.println("Example of TreeSet");
+        numberTreeSet.parallelStream().map(e -> e + " ").forEach(System.out::print);
+    }
+
+    private static void populateSet(Set set) {
+        set.add(1);
+        set.add(3);
+        set.add(2);
+        set.add(2);
+        set.add(6);
+        set.add(5);
+        set.add(7);
+        set.add(4);
     }
 }
